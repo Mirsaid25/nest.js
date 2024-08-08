218269b83c899ad6b9f60f5d092c50b6206f7632
Mirsaid25 <siddikovmirsaid01@gmail.com>
2024-08-08 14:53:12 +0500

    full project

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { BelongsToMany, Column, DataType, HasMany, Model, Table } from "sequelize-typescript";
 import { Role } from "src/roles/roles.model";
 import { UserRoles } from "../roles/user-roles.model";
+import { Post } from "src/posts/posts.model";
 
 interface UserCreationAttrs {
     email: string,
@@ -9,27 +10,31 @@ interface UserCreationAttrs {
 }
 
 @Table({ tableName: 'users' })
-export class User extends Model<User,UserCreationAttrs> {
-    @ApiProperty({example:"1", description:"Уникальный идентификатор"})
+export class User extends Model<User, UserCreationAttrs> {
+    @ApiProperty({ example: "1", description: "Уникальный идентификатор" })
     @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
     id: number;
 
-    @ApiProperty({example:"User@gmail.com", description:"Почтовый адрес"})
+    @ApiProperty({ example: "User@gmail.com", description: "Почтовый адрес" })
     @Column({ type: DataType.STRING, unique: true, allowNull: false })
     email: string;
 
-    @ApiProperty({example:"12345678", description:"Пароль"})
+    @ApiProperty({ example: "12345678", description: "Пароль" })
     @Column({ type: DataType.STRING, allowNull: false })
     password: string;
 
-    @ApiProperty({example:"true", description:"Забанен или нет"})
+    @ApiProperty({ example: "true", description: "Забанен или нет" })
     @Column({ type: DataType.BOOLEAN, defaultValue: false })
     banned: boolean;
 
-    @ApiProperty({example:"За хулиганство", description:"Причина блокировки"})
+    @ApiProperty({ example: "За хулиганство", description: "Причина блокировки" })
     @Column({ type: DataType.STRING, allowNull: true })
     banReason: string
 
     @BelongsToMany(() => Role, () => UserRoles)
     roles: Role[];
+
+    @HasMany(() => Post)
+    posts: Post[];
+
 }
